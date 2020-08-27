46fc363a03d4853a106ebeaf737a89f6ac5e2e31
Christopher Batey <christopher.batey@gmail.com>
2020-08-27 15:10:06 +0100

    Use bootstrap and akka management in integration spec
    This better tests local setup and avoids the override for testing
    comment in the tutorial

--- docs-source/docs/modules/shopping-microservices/examples/shopping-cart-service-scala/src/main/scala/shopping/cart/Main.scala
+++ docs-source/docs/modules/shopping-microservices/examples/shopping-cart-service-scala/src/main/scala/shopping/cart/Main.scala
@@ -53,9 +53,8 @@ object Main {
 
 class Main(context: ActorContext[Nothing]) extends AbstractBehavior[Nothing](context) {
   val system = context.system
-
-  startAkkaManagement()
-
+  AkkaManagement(system).start()
+  ClusterBootstrap(system).start()
   ShoppingCart.init(system)
 
   // tag::ItemPopularityProjection[]
@@ -90,12 +89,6 @@ class Main(context: ActorContext[Nothing]) extends AbstractBehavior[Nothing](con
     orderServiceClient
   }
   // end::SendOrderProjection[]
-
-  // can be overridden in tests
-  protected def startAkkaManagement(): Unit = {
-    AkkaManagement(system).start()
-    ClusterBootstrap(system).start()
-  }
 
   override def onMessage(msg: Nothing): Behavior[Nothing] = {
     this
