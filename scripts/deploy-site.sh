b498f3b1b3ae2787a2a377c3f8a785a8abac1144
Enno <458526+ennru@users.noreply.github.com>
2020-10-14 10:30:44 +0200

    Site deploy: correct dir to copy (#289)

--- scripts/deploy-site.sh
+++ scripts/deploy-site.sh
@@ -3,4 +3,4 @@ make html-author-mode
 eval "$(ssh-agent -s)"
 echo $SCP_SECRET | base64 -d > /tmp/id_rsa
 chmod 600 /tmp/id_rsa
-scp -i /tmp/id_rsa -o UserKnownHostsFile=.travis/known_hosts -r target/staging/* akkarepo@gustav.akka.io:akka.io/platform-guide
+scp -i /tmp/id_rsa -o UserKnownHostsFile=.travis/known_hosts -r target/* akkarepo@gustav.akka.io:akka.io/platform-guide
