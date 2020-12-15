8dc06e477772a79a42bdabb2bc41dd7a9cef3836
Enno Runne <458526+ennru@users.noreply.github.com>
2020-12-15 13:56:51 +0100

    Deploy WIP branch to akka.io (#424)

--- scripts/deploy-site.sh
+++ scripts/deploy-site.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-make all
+make build-wip
 eval "$(ssh-agent -s)"
 echo $SCP_SECRET | base64 -d > /tmp/id_rsa
 chmod 600 /tmp/id_rsa
-scp -i /tmp/id_rsa -o UserKnownHostsFile=.travis/known_hosts -r target/* akkarepo@gustav.akka.io:akka.io/platform-guide
+scp -i /tmp/id_rsa -o UserKnownHostsFile=.travis/known_hosts -r target/* akkarepo@gustav.akka.io:akka.io/platform-guide/wip/
