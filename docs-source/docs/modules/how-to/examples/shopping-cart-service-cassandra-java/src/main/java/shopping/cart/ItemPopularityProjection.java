46461913be84e48872f5ba12d528062c84ef17aa
Patrik Nordwall <patrik.nordwall@gmail.com>
2021-01-18 13:59:30 +0100

    How-to use Cassandra instead of RDBMS, #429 (#469)
    * How-to use Cassandra instead of RDBMS, #429
    
    * mvn bom

---
+++ docs-source/docs/modules/how-to/examples/shopping-cart-service-cassandra-java/src/main/java/shopping/cart/ItemPopularityProjection.java
@@ -0,0 +1,49 @@
+// tag::projection[]
+package shopping.cart;
+
+import akka.actor.typed.ActorSystem;
+import akka.cluster.sharding.typed.ShardedDaemonProcessSettings;
+import akka.cluster.sharding.typed.javadsl.ShardedDaemonProcess;
+import akka.persistence.cassandra.query.javadsl.CassandraReadJournal;
+import akka.persistence.query.Offset;
+import akka.projection.ProjectionBehavior;
+import akka.projection.ProjectionId;
+import akka.projection.cassandra.javadsl.CassandraProjection;
+import akka.projection.eventsourced.EventEnvelope;
+import akka.projection.eventsourced.javadsl.EventSourcedProvider;
+import akka.projection.javadsl.AtLeastOnceProjection;
+import akka.projection.javadsl.SourceProvider;
+import java.util.Optional;
+
+public final class ItemPopularityProjection {
+
+  private ItemPopularityProjection() {}
+
+  public static void init(ActorSystem<?> system, ItemPopularityRepository repository) {
+    ShardedDaemonProcess.get(system)
+        .init( // <1>
+            ProjectionBehavior.Command.class,
+            "ItemPopularityProjection",
+            ShoppingCart.TAGS.size(),
+            index -> ProjectionBehavior.create(createProjectionFor(system, repository, index)),
+            ShardedDaemonProcessSettings.create(system),
+            Optional.of(ProjectionBehavior.stopMessage()));
+  }
+
+  private static AtLeastOnceProjection<Offset, EventEnvelope<ShoppingCart.Event>>
+      createProjectionFor(ActorSystem<?> system, ItemPopularityRepository repository, int index) {
+    String tag = ShoppingCart.TAGS.get(index); // <2>
+
+    SourceProvider<Offset, EventEnvelope<ShoppingCart.Event>> sourceProvider = // <3>
+        EventSourcedProvider.eventsByTag(
+            system,
+            CassandraReadJournal.Identifier(), // <4>
+            tag);
+
+    return CassandraProjection.atLeastOnce( // <5>
+        ProjectionId.of("ItemPopularityProjection", tag),
+        sourceProvider,
+        () -> new ItemPopularityProjectionHandler(tag, repository)); // <6>
+  }
+}
+// end::projection[]
--- scripts/prepare-downloads.sh
+++ scripts/prepare-downloads.sh
@@ -3,6 +3,9 @@
 declare -r tutorial_sources="${PWD}/docs-source/docs/modules/microservices-tutorial/examples"
 declare -r tutorial_attachments="${PWD}/docs-source/docs/modules/microservices-tutorial/assets/attachments"
 
+declare -r howto_sources="${PWD}/docs-source/docs/modules/how-to/examples"
+declare -r howto_attachments="${PWD}/docs-source/docs/modules/how-to/assets/attachments"
+
 declare -r temporal_folder="${PWD}/target/zips"
 declare -r temporal_attachments="${PWD}/target/microservices-tutorial/_attachments"
 mkdir -p ${temporal_attachments}
@@ -34,12 +37,12 @@ function prepareTemporalFolder() {
 ## Copy a folder with some code into the temporal folder. The 
 ## copied folder will be renamed to the folder name we want the 
 ## user to see when unzipping the file.
-##   source_name -> subfolder in `microservices-tutorial/examples`
+##   source_name -> folder in `examples`
 ##   target_name ->  folder name the user should see (must not use a numeric prefix of a laguage suffix)
 function fetchProject() {
    source_name=$1
    target_name=$2
-   cp -a ${tutorial_sources}/${source_name} ${temporal_folder}/${target_name}
+   cp -a ${source_name} ${temporal_folder}/${target_name}
 }
 
 ## Zip the contents in $temporal_folder and create the 
@@ -48,8 +51,8 @@ function zipAndAttach() {
    zip_name=$1
    pushd ${temporal_folder}
    removeTags
-   zip --quiet -r ${tutorial_attachments}/${zip_name} *
-   cp ${tutorial_attachments}/${zip_name} ${temporal_attachments}
+   zip --quiet -r ${zip_name} *
+   cp ${zip_name} ${temporal_attachments}
    popd
 }
 
@@ -57,100 +60,110 @@ function zipAndAttach() {
 ## Scala Zip files
 ## empty seed template
 prepareTemporalFolder
-fetchProject 00-shopping-cart-service-scala shopping-cart-service
-fetchProject 00-shopping-analytics-service-scala shopping-analytics-service
-fetchProject 00-shopping-order-service-scala shopping-order-service
-zipAndAttach 0-shopping-cart-start-scala.zip
+fetchProject ${tutorial_sources}/00-shopping-cart-service-scala shopping-cart-service
+fetchProject ${tutorial_sources}/00-shopping-analytics-service-scala shopping-analytics-service
+fetchProject ${tutorial_sources}/00-shopping-order-service-scala shopping-order-service
+zipAndAttach ${tutorial_attachments}/0-shopping-cart-start-scala.zip
 
 ## gRPC service
 prepareTemporalFolder
-fetchProject 01-shopping-cart-service-scala shopping-cart-service
-fetchProject 00-shopping-analytics-service-scala shopping-analytics-service
-fetchProject 00-shopping-order-service-scala shopping-order-service
-zipAndAttach 1-shopping-cart-grpc-scala.zip
+fetchProject ${tutorial_sources}/01-shopping-cart-service-scala shopping-cart-service
+fetchProject ${tutorial_sources}/00-shopping-analytics-service-scala shopping-analytics-service
+fetchProject ${tutorial_sources}/00-shopping-order-service-scala shopping-order-service
+zipAndAttach ${tutorial_attachments}/1-shopping-cart-grpc-scala.zip
 
 ## basic entity
 prepareTemporalFolder
-fetchProject 02-shopping-cart-service-scala shopping-cart-service
-fetchProject 00-shopping-analytics-service-scala shopping-analytics-service
-fetchProject 00-shopping-order-service-scala shopping-order-service
-zipAndAttach 2-shopping-cart-event-sourced-scala.zip
+fetchProject ${tutorial_sources}/02-shopping-cart-service-scala shopping-cart-service
+fetchProject ${tutorial_sources}/00-shopping-analytics-service-scala shopping-analytics-service
+fetchProject ${tutorial_sources}/00-shopping-order-service-scala shopping-order-service
+zipAndAttach ${tutorial_attachments}/2-shopping-cart-event-sourced-scala.zip
 
 ## complete entity
 prepareTemporalFolder
-fetchProject 03-shopping-cart-service-scala shopping-cart-service
-fetchProject 00-shopping-analytics-service-scala shopping-analytics-service
-fetchProject 00-shopping-order-service-scala shopping-order-service
-zipAndAttach 3-shopping-cart-event-sourced-complete-scala.zip
+fetchProject ${tutorial_sources}/03-shopping-cart-service-scala shopping-cart-service
+fetchProject ${tutorial_sources}/00-shopping-analytics-service-scala shopping-analytics-service
+fetchProject ${tutorial_sources}/00-shopping-order-service-scala shopping-order-service
+zipAndAttach ${tutorial_attachments}/3-shopping-cart-event-sourced-complete-scala.zip
 
 ## projection query
 prepareTemporalFolder
-fetchProject 04-shopping-cart-service-scala shopping-cart-service
-fetchProject 00-shopping-analytics-service-scala shopping-analytics-service
-fetchProject 00-shopping-order-service-scala shopping-order-service
-zipAndAttach 4-shopping-cart-projection-scala.zip
+fetchProject ${tutorial_sources}/04-shopping-cart-service-scala shopping-cart-service
+fetchProject ${tutorial_sources}/00-shopping-analytics-service-scala shopping-analytics-service
+fetchProject ${tutorial_sources}/00-shopping-order-service-scala shopping-order-service
+zipAndAttach ${tutorial_attachments}/4-shopping-cart-projection-scala.zip
 
 ## projection kafka
 prepareTemporalFolder
-fetchProject 05-shopping-cart-service-scala shopping-cart-service
-fetchProject shopping-analytics-service-scala shopping-analytics-service
-fetchProject 00-shopping-order-service-scala shopping-order-service
-zipAndAttach 5-shopping-cart-projection-kafka-scala.zip
+fetchProject ${tutorial_sources}/05-shopping-cart-service-scala shopping-cart-service
+fetchProject ${tutorial_sources}/shopping-analytics-service-scala shopping-analytics-service
+fetchProject ${tutorial_sources}/00-shopping-order-service-scala shopping-order-service
+zipAndAttach ${tutorial_attachments}/5-shopping-cart-projection-kafka-scala.zip
 
 ## complete
 prepareTemporalFolder
-fetchProject shopping-cart-service-scala shopping-cart-service
-fetchProject shopping-analytics-service-scala shopping-analytics-service
-fetchProject shopping-order-service-scala shopping-order-service
-zipAndAttach 6-shopping-cart-complete-scala.zip
+fetchProject ${tutorial_sources}/shopping-cart-service-scala shopping-cart-service
+fetchProject ${tutorial_sources}/shopping-analytics-service-scala shopping-analytics-service
+fetchProject ${tutorial_sources}/shopping-order-service-scala shopping-order-service
+zipAndAttach ${tutorial_attachments}/6-shopping-cart-complete-scala.zip
+
+## how-to Cassandra
+prepareTemporalFolder
+fetchProject ${howto_sources}/shopping-cart-service-cassandra-scala shopping-cart-service
+zipAndAttach ${howto_attachments}/shopping-cart-cassandra-scala.zip
 
 
 ## Java Zip files
 ## empty seed template
 prepareTemporalFolder
-fetchProject 00-shopping-cart-service-java shopping-cart-service
-fetchProject 00-shopping-analytics-service-java shopping-analytics-service
-fetchProject 00-shopping-order-service-java shopping-order-service
-zipAndAttach 0-shopping-cart-start-java.zip
+fetchProject ${tutorial_sources}/00-shopping-cart-service-java shopping-cart-service
+fetchProject ${tutorial_sources}/00-shopping-analytics-service-java shopping-analytics-service
+fetchProject ${tutorial_sources}/00-shopping-order-service-java shopping-order-service
+zipAndAttach ${tutorial_attachments}/0-shopping-cart-start-java.zip
 
 ## gRPC service
 prepareTemporalFolder
-fetchProject 01-shopping-cart-service-java shopping-cart-service
-fetchProject 00-shopping-analytics-service-java shopping-analytics-service
-fetchProject 00-shopping-order-service-java shopping-order-service
-zipAndAttach 1-shopping-cart-grpc-java.zip
+fetchProject ${tutorial_sources}/01-shopping-cart-service-java shopping-cart-service
+fetchProject ${tutorial_sources}/00-shopping-analytics-service-java shopping-analytics-service
+fetchProject ${tutorial_sources}/00-shopping-order-service-java shopping-order-service
+zipAndAttach ${tutorial_attachments}/1-shopping-cart-grpc-java.zip
 
 ## basic entity
 prepareTemporalFolder
-fetchProject 02-shopping-cart-service-java shopping-cart-service
-fetchProject 00-shopping-analytics-service-java shopping-analytics-service
-fetchProject 00-shopping-order-service-java shopping-order-service
-zipAndAttach 2-shopping-cart-event-sourced-java.zip
+fetchProject ${tutorial_sources}/02-shopping-cart-service-java shopping-cart-service
+fetchProject ${tutorial_sources}/00-shopping-analytics-service-java shopping-analytics-service
+fetchProject ${tutorial_sources}/00-shopping-order-service-java shopping-order-service
+zipAndAttach ${tutorial_attachments}/2-shopping-cart-event-sourced-java.zip
 
 ## complete entity
 prepareTemporalFolder
-fetchProject 03-shopping-cart-service-java shopping-cart-service
-fetchProject 00-shopping-analytics-service-java shopping-analytics-service
-fetchProject 00-shopping-order-service-java shopping-order-service
-zipAndAttach 3-shopping-cart-event-sourced-complete-java.zip
+fetchProject ${tutorial_sources}/03-shopping-cart-service-java shopping-cart-service
+fetchProject ${tutorial_sources}/00-shopping-analytics-service-java shopping-analytics-service
+fetchProject ${tutorial_sources}/00-shopping-order-service-java shopping-order-service
+zipAndAttach ${tutorial_attachments}/3-shopping-cart-event-sourced-complete-java.zip
 
 ## projection query
 prepareTemporalFolder
-fetchProject 04-shopping-cart-service-java shopping-cart-service
-fetchProject 00-shopping-analytics-service-java shopping-analytics-service
-fetchProject 00-shopping-order-service-java shopping-order-service
-zipAndAttach 4-shopping-cart-projection-java.zip
+fetchProject ${tutorial_sources}/04-shopping-cart-service-java shopping-cart-service
+fetchProject ${tutorial_sources}/00-shopping-analytics-service-java shopping-analytics-service
+fetchProject ${tutorial_sources}/00-shopping-order-service-java shopping-order-service
+zipAndAttach ${tutorial_attachments}/4-shopping-cart-projection-java.zip
 
 ## projection kafka
 prepareTemporalFolder
-fetchProject 05-shopping-cart-service-java shopping-cart-service
-fetchProject shopping-analytics-service-java shopping-analytics-service
-fetchProject 00-shopping-order-service-java shopping-order-service
-zipAndAttach 5-shopping-cart-projection-kafka-java.zip
+fetchProject ${tutorial_sources}/05-shopping-cart-service-java shopping-cart-service
+fetchProject ${tutorial_sources}/shopping-analytics-service-java shopping-analytics-service
+fetchProject ${tutorial_sources}/00-shopping-order-service-java shopping-order-service
+zipAndAttach ${tutorial_attachments}/5-shopping-cart-projection-kafka-java.zip
 
 ## complete
 prepareTemporalFolder
-fetchProject shopping-cart-service-java shopping-cart-service
-fetchProject shopping-analytics-service-java shopping-analytics-service
-fetchProject shopping-order-service-java shopping-order-service
-zipAndAttach 6-shopping-cart-complete-java.zip
+fetchProject ${tutorial_sources}/shopping-cart-service-java shopping-cart-service
+fetchProject ${tutorial_sources}/shopping-analytics-service-java shopping-analytics-service
+fetchProject ${tutorial_sources}/shopping-order-service-java shopping-order-service
+zipAndAttach ${tutorial_attachments}/6-shopping-cart-complete-java.zip
+
+## how-to Cassandra
+prepareTemporalFolder
+fetchProject ${howto_sources}/shopping-cart-service-cassandra-java shopping-cart-service
+zipAndAttach ${howto_attachments}/shopping-cart-cassandra-java.zip
