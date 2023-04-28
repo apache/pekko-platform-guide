828e2626b81644b6c659c4f2fb1811f4efe42523
Sam Byng <sam.byng@gmail.com>
2023-04-28 14:13:21 +0100

    244: Setup for pekko: Remove the deploy-wip workflow and modify the deploy-site script
    The gustav VM was used for test purposes. In akka there was  long-lifetime akka.io website that main pipeline deploy-wip workflows would continuously deploy to. This will not be the case for pekko-platform guides so this is removed here.
    
    I have kept the deploy-site script in case anyone wants to host their own local VM TEST_DEPLOYMENT_VM and deploy the site onto that.

--- scripts/deploy-site.sh
+++ scripts/deploy-site.sh
@@ -7,4 +7,5 @@ make build-wip
 eval "$(ssh-agent -s)"
 echo "${SCP_SECRET}" > /tmp/id_rsa
 chmod 600 /tmp/id_rsa
-scp -i /tmp/id_rsa -o UserKnownHostsFile=.travis/known_hosts -r target/* akkarepo@gustav.akka.io:akka.io/platform-guide/wip/
+# To be determined: Set up a test VM TEST_VM_HOSTNAME to deploy site into 
+scp -i /tmp/id_rsa -o UserKnownHostsFile=.travis/known_hosts -r target/* pekkorepo@<TEST_VM_HOSTNAME>:pekko.apache.org/docs/pekko-platform-guide/wip/
