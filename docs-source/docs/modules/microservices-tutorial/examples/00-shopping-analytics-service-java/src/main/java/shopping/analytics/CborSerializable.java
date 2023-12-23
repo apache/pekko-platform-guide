ab726b2a1f674d1648d42a254cb826e91a5a2469
PJ Fanning <pjfanning@users.noreply.github.com>
2023-12-23 18:13:22 +0100

    switch 1st sample to pekko

--- docs-source/docs/modules/microservices-tutorial/examples/00-shopping-analytics-service-java/src/main/java/shopping/analytics/CborSerializable.java
+++ docs-source/docs/modules/microservices-tutorial/examples/00-shopping-analytics-service-java/src/main/java/shopping/analytics/CborSerializable.java
@@ -2,6 +2,6 @@ package shopping.analytics;
 
 /**
  * Marker trait for serialization with Jackson CBOR. Enabled in serialization.conf
- * `akka.actor.serialization-bindings` (via application.conf).
+ * `pekko.actor.serialization-bindings` (via application.conf).
  */
 public interface CborSerializable {}
